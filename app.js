c07cd733f9d3bc2122c6ecc9180505bb9734d327
SpeedyProgrammerX <devanshdesai29@gmail.com>
2018-10-13 20:35:02 -0400

    Making new apis for testing, creating, logging in the user.

--- app.js
+++ app.js
@@ -25,6 +25,121 @@ app.get("/", (req, res) => {
     res.status(200).send("Hello World");
 });
 
+app.post("/addUser", (req, res) => {
+
+    var userId = req.body.username;
+    var user = db.collection('users').doc(userId);
+
+    var setUser = user.set({
+        achievements: ["Joined the Community"],
+        country: req.body.country,
+        email: req.body.email,
+        password: req.body.password,
+        level: 1,
+        points: 0,
+        zipcode: req.body.zipcode,
+        name: req.body.name
+    });
+
+    res.send("Successfully Created the user");
+
+});
+
+app.post("/login", (req, res) => {
+
+    var userId = req.body.username;
+    var password = req.body.password;
+
+    db.collection('users').doc(userId).get()
+    .then((snapshot) => {    
+        
+        if(password == snapshot.data().password){
+            res.send({"status" : "Successful Login"});
+        }else{
+            res.status(401).send({"status" : "Error"});
+        }
+        
+    })
+    .catch((err) => {
+        res.status(400).send("Error");
+        console.log('Error getting documents', err);
+    });
+    
+});
+
+app.get("/profile", (req, res) => {
+
+    var userId = req.header('username');
+    db.collection('users').doc(userId).get()
+    .then((snapshot) => {    
+        
+        res.send(snapshot.data());
+    })
+    .catch((err) => {
+        res.status(400).send("Error");
+        console.log('Error getting documents', err);
+    });
+
+});
+
+app.get("/profile", (req, res) => {
+
+    var userId = req.header('username');
+    db.collection('users').doc(userId).get()
+    .then((snapshot) => {    
+        
+        res.send(snapshot.data());
+    })
+    .catch((err) => {
+        res.status(400).send("Error");
+        console.log('Error getting documents', err);
+    });
+
+});
+
+app.get("/getAllUsers", (req, res) => {
+
+    db.collection('users').get()
+    .then((snapshot) => {
+        var response = []
+        snapshot.forEach((doc) => {
+            // console.log(doc.id, '=>', doc.data());
+            var body = {
+                'username' : doc.id,
+                'profile' : _.pick(doc.data(), "name", "email", "country", "zipcode")
+            }
+            response.push(body)
+        });
+        res.send(response);
+    })
+    .catch((err) => {
+        res.status(400).send("Error");
+        console.log('Error getting documents', err);
+    });
+
+});
+
+app.get("/test", (req, res) => {
+
+    db.collection('users').get()
+    .then((snapshot) => {
+        snapshot.forEach((doc) => {
+            console.log(doc.id, '=>', doc.data());
+            var x = doc.id
+            var body = {
+                x : doc.data()
+            }
+            res.send(body);
+        });
+    })
+    .catch((err) => {
+        res.status(400).send("Error");
+        console.log('Error getting documents', err);
+    });
+
+
+});
+
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
